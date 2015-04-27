34425cc7ad13f711c50fc99ab986d76d48d989a6
Amanda Konopko <amanda.konopko@blinkreaction.com>
2015-04-27 13:23:05 -0400

    Demo Working
    Created a working demo of the snappish framework.

---
+++ js/global.js
@@ -0,0 +1,8 @@
+$('#wrapper').snappish();
+
+$('#wrapper').on('scrollbegin.snappish', function(event, data) {
+  console.log('from slide', data.fromSlide, data.fromSlideNum);
+  console.log('to slide', data.toSlide, data.toSlideNum);
+  data.fromSlide.removeClass('active');
+  data.toSlide.addClass('active');
+});
